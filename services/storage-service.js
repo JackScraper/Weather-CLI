bb1a261de1296667f3cb67266b84748e09f11e27
jackscraper <ivankrylov322@gmail.com>
2023-04-12 16:20:52 +0300

    Stable Build

--- services/storage-service.js
+++ services/storage-service.js
@@ -25,7 +25,7 @@ const getKeyValue = async (key) => {
         const data = JSON.parse(file);
         return data[key];
     }
-    return undefined;
+    return 'Select City!';
 };
 
 const isExist = async (path) => {
